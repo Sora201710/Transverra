c16d392a4ad1b490de56380db0011ee1156ab87d
Christopher Roy <chrisroybd@gmail.com>
2026-02-09 17:56:14 -0500

    changed project structure

--- backend/src/utils/translation/translate.py
+++
@@ -1,5 +0,0 @@
-# TODO: come up with a solution for getting rate limited by the translation API
-#       - add a delay (customizable by user)
-#       - retry a request after a delay if it gets rate limited
-#       - worst case need to host my own LLM
-# selenium probably won't work due to this reason
