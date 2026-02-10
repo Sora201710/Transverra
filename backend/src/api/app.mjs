46f6a7cfa3112b01033c3b9dfe736139375e8cce
Christopher Roy <chrisroybd@gmail.com>
2026-02-10 17:09:03 -0500

    fixed CORS and added communicated with frontend

---
+++ backend/src/api/app.mjs
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+import path from "path";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// load root .env
+dotenv.config({
+  path: path.resolve(process.cwd(), "../../../.env"),
+});
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_HOST,
+    optionsSuccessStatus: 200,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Hello World!",
+  });
+});
+
+// TODO: protect this endpoint so that only frontend or celery can send requests to this directly
+// TODO: validate data
+
+app.post("/api/translate", express.urlencoded(), (req, res) => {
+  res.json({
+    message: "received form data",
+  });
+  // takes in:
+  // file (html for now)
+  // source language
+  // target language
+  // novel source
+});
+
+app.listen(process.env.API_PORT, () => {
+  console.log(
+    `Transvera API hosted on: ${process.env.API_HOST}:${process.env.API_PORT}`,
+  );
+});
