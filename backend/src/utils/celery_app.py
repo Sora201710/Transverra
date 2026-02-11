6cfa7a6932c149ca15b5c3298803125ac55537e8
Christopher Roy <chrisroybd@gmail.com>
2026-02-11 01:37:15 -0500

    setup celery worker

---
+++ backend/src/utils/celery_app.py
@@ -0,0 +1,21 @@
+from celery import Celery
+import os
+from dotenv import load_dotenv
+
+load_dotenv("../../../.env")
+
+REDIS_URL = os.getenv("REDIS_URL")
+app = Celery("tasks", broker=REDIS_URL)
+
+# TODO: use RPC to send real time state changes
+# to frontend or express server
+
+# TODO: setup a fastapi python server
+# to handle all the translation requests
+# then make it communicate with express
+# server
+
+
+@app.task
+def add(x, y):
+    return x + y
